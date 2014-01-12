cb4b0cd9dfc06ba160823e9a3d32f885ffe801ef
Dan Tao <daniel.tao@gmail.com>
2014-01-11 21:55:34 -0800

    added logic to populate e-mails when creating a message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,11 @@
 class Message < ActiveRecord::Base
   belongs_to :sender, :class_name => 'User'
   belongs_to :recipient, :class_name => 'User'
+
+  before_create :populate_emails
+
+  def populate_emails
+    self.sender_email ||= "#{self.sender.user_name}@publicinbox.net" if self.sender_id.present?
+    self.recipient_email ||= "#{self.recipient.user_name}@publicinbox" if self.recipient_id.present?
+  end
 end
