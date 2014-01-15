1e24ee62873d58ee578119f8897b99f1e347dd20
Dan Tao <daniel.tao@gmail.com>
2014-01-15 09:10:19 -0800

    added message view

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -7,4 +7,17 @@ var messagesController = mainApp.controller('MessagesCtrl', ['$scope', '$http', 
     $scope.outbox = data.outbox;
   });
 
+  $scope.showMessage = function showMessage(message, e) {
+    e.preventDefault();
+
+    $scope.message = angular.extend({}, message, {
+      preposition: message.sender_email ? 'from' : 'to',
+      display_email: message.sender_email || message.recipient_email
+    });
+  };
+
+  $scope.closeMessage = function closeMessage() {
+    $scope.message = null;
+  };
+
 }]);
