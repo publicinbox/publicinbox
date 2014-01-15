dfe58829116f697a05f971d69052b3295c16f5b8
Dan Tao <daniel.tao@gmail.com>
2014-01-15 12:29:45 -0800

    working on a slightly different approach for navigation

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,45 +1,79 @@
 var publicInboxApp = angular.module('publicInboxApp', ['ngSanitize']);
 
-publicInboxApp.directive('piTabs', function() {
+publicInboxApp.directive('piNav', function() {
+
+  var sections = {};
 
   return {
     link: function(scope, element) {
-      var navLinks = element.find('ul li a'),
-          sections = $(element.attr('data-sections'));
+      var navLinks = element.find('ul li a');
 
-      function showSection(section) {
-        // Hide all but the target section
-        sections.hide();
-        section.show();
-
-        // Update the page title
-        scope.title = section.attr('data-title');
-        scope.$apply();
-      }
-
-      navLinks.on('click', function(e) {
-        var href   = $(this).attr('href'),
-            target = $(href);
+      var sections = {};
+      angular.forEach(navLinks, function(link) {
+        var target = $(link).attr('href');
 
         // Only apply this directive to links to sections
-        if (href.charAt(0) !== '#') {
+        if (target.charAt(0) !== '#') {
+          return;
+        }
+
+        var section = $(target),
+            id      = section.attr('id'),
+            title   = section.attr('data-title');
+
+        sections[id] = {
+          title: title
+        };
+      });
+
+      scope.sections = angular.extend(sections, {
+        message: {
+          title: function() {
+            return 'Message from ' + scope.message.display_email;
+          }
+        }
+      });
+
+      navLinks.on('click', function(e) {
+        var target = $(this).attr('href');
+
+        // Only apply this directive to links to sections
+        if (target && target.charAt(0) !== '#') {
           return;
         }
 
         // Prevent the browser from updating the address bar
         e.preventDefault();
 
-        showSection(target);
+        scope.showSection(target);
+        scope.$apply();
       });
 
-      // Show the first action on page load
-      showSection(sections.first());
+      var firstSection = navLinks.first().attr('href');
+      scope.showSection(firstSection);
     }
   };
 
 });
 
 var mainController = publicInboxApp.controller('MainCtrl', ['$scope', function($scope) {
+
+  $scope.showSection = function showSection(sectionName) {
+    if (sectionName.charAt(0) === '#') {
+      sectionName = sectionName.substring(1);
+    }
+
+    var title = $scope.sections[sectionName].title;
+    $scope.title = typeof title === 'function' ? title() : title;
+    $scope.activeSection = sectionName;
+  };
+
+  $scope.isActiveSection = function isActiveSection(sectionName) {
+    return $scope.activeSection === sectionName;
+  };
+
+  $scope.activeSection = 'inbox';
+
 }]);
 
 var messagesController = publicInboxApp.controller('MessagesCtrl', ['$scope', '$http', function($scope, $http) {
@@ -56,10 +90,8 @@ var messagesController = publicInboxApp.controller('MessagesCtrl', ['$scope', '$
       preposition: message.sender_email ? 'from' : 'to',
       display_email: message.sender_email || message.recipient_email
     });
-  };
 
-  $scope.closeMessage = function closeMessage() {
-    $scope.message = null;
+    $scope.showSection('message');
   };
 
 }]);
