7cbe6a7c8e5f55891518418b028d4bd073e6640a
Dan Tao <daniel.tao@gmail.com>
2014-01-17 10:12:44 -0800

    create links automatically in e-mails

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -6,7 +6,9 @@ module ApiHelper
     include Redcarpet::Render::SmartyPants
   end
 
-  MARKDOWN_RENDERER = Redcarpet::Markdown.new(SmartHTML.new(:hard_wrap => true))
+  MARKDOWN_RENDERER = Redcarpet::Markdown.new(SmartHTML.new(:hard_wrap => true), {
+    :autolink => true
+  })
 
   def profile_image(email, options={})
     gravatar_image_url(email, { :size => 50, :default => 'identicon' }.merge(options))
