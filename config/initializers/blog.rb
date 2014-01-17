4b27813757dd1a189c2ce00c567f35e05c4ba1c3
Dan Tao <daniel.tao@gmail.com>
2014-01-17 07:15:03 -0800

    use redcarpet instead of rdiscount for rendering blog posts

--- config/initializers/blog.rb
+++ config/initializers/blog.rb
@@ -1,1 +1,15 @@
+# Configure markdown-rails to use Redcarpet w/ SmartyPants
+# rather than rdiscount
+MarkdownRails.configure do |config|
+  class SmartyPantsRenderer < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
+  end
+
+  markdown = Redcarpet::Markdown.new(SmartyPantsRenderer)
+
+  config.render do |markdown_source|
+    markdown.render(markdown_source)
+  end
+end
+
 Blog.load_all_posts!
