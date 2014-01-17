7b693ca89b513acaf5c04070a3a55c4b6c380f43
Dan Tao <daniel.tao@gmail.com>
2014-01-16 16:32:01 -0800

    hard wrap that Markdown!

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -2,12 +2,17 @@ module ApiHelper
   include ActionView::Helpers::DateHelper
   include GravatarImageTag
 
+  class SmartHTML < Redcarpet::Render::HTML
+    include Redcarpet::Render::SmartyPants
+  end
+
+  MARKDOWN_RENDERER = Redcarpet::Markdown.new(SmartHTML.new(:hard_wrap => true))
+
   def profile_image(email, options={})
     gravatar_image_url(email, { :size => 50, :default => 'identicon' }.merge(options))
   end
 
   def markdown(text)
-    @renderer ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML)
-    @renderer.render(text || '')
+    MARKDOWN_RENDERER.render(text || '')
   end
 end
