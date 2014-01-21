be9cacc74a3a9886329776a83c71ff62339d87ea
Dan Tao <daniel.tao@gmail.com>
2014-01-21 09:43:52 -0800

    updated app.js to intelligently load only available modules

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,28 @@
-var publicInboxApp = angular.module('publicInboxApp', ['ngSanitize', 'ui.codemirror', 'ui.bootstrap']);
+// It seems the only way to check for the existence of a module
+// is to catch an exception when trying to load it?
+// http://stackoverflow.com/questions/19206553
+function moduleExists(moduleName) {
+  try {
+    angular.module(moduleName);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
+
+// The idea here is to just load the modules that are currently defined, mainly
+// to handle the issue of not having a separate version of everything for users
+// vs. guests.
+function getModules() {
+  var modules = [];
+
+  angular.forEach(['ngSanitize', 'ui.codemirror', 'ui.bootstrap'], function(moduleName) {
+    if (moduleExists(moduleName)) {
+      modules.push(moduleName);
+    }
+  });
+
+  return modules;
+}
+
+var publicInboxApp = angular.module('publicInboxApp', getModules());
--- app/assets/javascripts/guest.js
+++ app/assets/javascripts/guest.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require angular
 //= require angular-sanitize
-//= require ui-codemirror
 //= require app
 //= require directives/pi-navigable-sections
 //= require controllers/main
