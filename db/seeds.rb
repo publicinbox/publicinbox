ac1cc9a0a25f44e5c7fc8b9ca081d9d5f61da352
Dan Tao <daniel.tao@gmail.com>
2014-05-01 08:16:53 -0700

    added seed script w/ a thread in there

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+@dan = User.find_by(:user_name => 'dan') || User.create!({
+  :user_name => 'dan',
+  :real_name => 'Dan Tao',
+  :password => 'passw0rd',
+  :password_confirmation => 'passw0rd'
+})
+
+Dir.glob(File.join(__dir__, 'seed', '*.json')).each do |file|
+  data = JSON.parse(File.read(file))
+  Message.create_from_external!(data)
+end
+
+# Create back-and-forth thread between me and myself
+
+def incoming(message)
+  Message.create!({
+    :thread_id => 'foo',
+    :sender_email => 'daniel.tao@gmail.com',
+    :recipient => @dan,
+    :subject => 'Hey Dan!',
+    :body => message
+  })
+end
+
+def outgoing(message)
+  Message.create!({
+    :thread_id => 'foo',
+    :sender => @dan,
+    :recipient_email => 'daniel.tao@gmail.com',
+    :subject => 'Hey Dan!',
+    :body => message
+  })
+end
+
+incoming("How's it going?")
+outgoing("It's going well, how about you?")
+incoming("Oh, pretty good, pretty good. Want to get coffee soon?")
+outgoing("You know it! How about tomorrow at Philz?")
+incoming("Sounds good, see you then.")
