19b6b8203fd585163ebe2410f1ed458afb7a3757
Dan Tao <daniel.tao@gmail.com>
2014-05-02 07:55:32 -0700

    removed logic to check for module existence

--- app/assets/javascripts/public_inbox.js
+++ app/assets/javascripts/public_inbox.js
@@ -1,31 +1,8 @@
-// It seems the only way to check for the existence of a module
-// is to catch an exception when trying to load it?
-// http://stackoverflow.com/questions/19206553
-function moduleExists(moduleName) {
-  try {
-    angular.module(moduleName);
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-// The idea here is to just load the modules that are currently defined, mainly
-// to handle the issue of not having a separate version of everything for users
-// vs. guests.
-function getModules() {
-  var modules = [];
-
-  angular.forEach(['ngSanitize', 'ui.codemirror', 'ui.bootstrap'], function(moduleName) {
-    if (moduleExists(moduleName)) {
-      modules.push(moduleName);
-    }
-  });
-
-  return modules;
-}
-
-var PublicInbox = angular.module('PublicInbox', getModules());
+var PublicInbox = angular.module('PublicInbox', [
+  'ngSanitize',
+  'ui.codemirror',
+  'ui.bootstrap'
+]);
 
 PublicInbox.filter('count', CountFilter);
 
